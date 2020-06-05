375e6c74fa9ff6d4cb2c51ba2973342ba2a54c49
Currt-Wei <2698230239@qq.com>
2020-06-05 02:03:10 +0000

    用户显示界面

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,8 +32,14 @@ class User extends Authenticatable
      * The attributes that should be cast to native types.
      *
      * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+         */
+        protected $casts = [
+            'email_verified_at' => 'datetime',
+        ];
+
+    public function gravatar($size='100'){
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
 }
