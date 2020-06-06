92b28a4e4aa827689de1880502d63a0612058c6e
Currt-Wei <2698230239@qq.com>
2020-06-06 03:10:05 +0000

    用户资料更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -57,7 +57,7 @@ class UsersController extends Controller
 
         $user->update($data);
 
-        session()->flash('success','修改成功！');
+        session()->flash('success','个人资料更新成功！');
 
         return redirect()->route('users.show', $user);
     }
