d7bb1847d189e252f761a041efa05f924b6cebb8
Currt-Wei <2698230239@qq.com>
2020-06-08 11:45:15 +0000

    发布微博

--- database/seeds/StatusesTableSeeder.php
+++ database/seeds/StatusesTableSeeder.php
@@ -14,7 +14,7 @@ class StatusesTableSeeder extends Seeder
     public function run()
     {
         //
-        $user_ids=['1','2','3'];
+        $user_ids=['1','2','3','51'];
         $faker = app(Faker\Generator::class);
 
         $statuses=factory(Status::class)->times(100)->make()->each(
